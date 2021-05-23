9c89d00e5e6db876ab84d76b5c416bfb6c44aa60
StanislauReut <52281305+StanislauReut@users.noreply.github.com>
2021-05-23 23:25:23 +0300

    finished homework

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,57 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    if ([ratingArray count] < 3) {
+        return 0;
+    }
+    
+    NSMutableArray *inputBuffer;
+    NSMutableArray *outputBuffer;
+    NSMutableSet *finalSet = [NSMutableSet new];
+    
+    for (int i = 0; i < [ratingArray count] - 2; i++) {
+        
+        inputBuffer = [[NSMutableArray alloc] initWithObjects:ratingArray[i], nil];
+        outputBuffer = [[NSMutableArray alloc] initWithObjects:ratingArray[i], nil];
+        
+        for (int j = i+1; j < [ratingArray count]; j++) {
+            if ([inputBuffer count] > 1) {
+                [inputBuffer removeLastObject];
+            }
+            
+            if ([outputBuffer count] > 1) {
+                [outputBuffer removeLastObject];
+            }
+            
+            if ([ratingArray[j] intValue] > [[inputBuffer lastObject] intValue]) {
+                [inputBuffer addObject:[ratingArray objectAtIndex:j]];
+            } else if ([[ratingArray objectAtIndex:j] intValue] < [[outputBuffer lastObject] intValue]) {
+                [outputBuffer addObject:[ratingArray objectAtIndex:j]];
+            }
+            
+            for (int k = j+1; k < [ratingArray count]; k++) {
+                if ([ratingArray[k] intValue] > [[inputBuffer lastObject] intValue]) {
+                    [inputBuffer addObject:[ratingArray objectAtIndex:k]];
+                } else if ([[ratingArray objectAtIndex:k] intValue] < [[outputBuffer lastObject] intValue]) {
+                    [outputBuffer addObject:[ratingArray objectAtIndex:k]];
+                }
+                
+                if ([inputBuffer count] > 2) {
+                    [finalSet addObject: [[inputBuffer valueForKey:@"description"] componentsJoinedByString:@""]];
+                    [inputBuffer removeLastObject];
+                }
+                
+                if ([outputBuffer count] > 2) {
+                    [finalSet addObject: [[outputBuffer valueForKey:@"description"] componentsJoinedByString:@""]];
+                    [outputBuffer removeLastObject];
+                }
+                
+            }
+            
+        }
+    }
+    return [finalSet count];
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,33 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSString *stringOne = [[NSString alloc] initWithString: string1];
+    NSString *stringTwo = [[NSString alloc] initWithString: string2];
+    NSMutableString *compositeString = [[NSMutableString alloc] init];
+    
+    while ([stringOne length] && [stringTwo length]) {
+        const char *first = [stringOne UTF8String];
+        const char *second = [stringTwo UTF8String];
+        
+        if (first[0] < second[0] || first[0] == second[0]) {
+            [compositeString appendString: [stringOne substringToIndex: 1]];
+            stringOne = [stringOne substringFromIndex: 1];
+        } else {
+            [compositeString appendString: [stringTwo substringToIndex: 1]];
+            stringTwo = [stringTwo substringFromIndex: 1];
+        }
+    }
+      
+    
+    if ([stringOne length]) {
+        [compositeString appendString: stringOne];
+    } else {
+        [compositeString appendString: stringTwo];
+    }
+    
+    
+    return compositeString;
 }
 
 @end
